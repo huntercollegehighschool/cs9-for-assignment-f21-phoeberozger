c2d615867925e3e98743d1d62856c38aae269fcc
Phoebe Rozger <95229083+phoeberozger@users.noreply.github.com>
2022-01-06 04:14:52 +0000

    completed

--- part2.py
+++ part2.py
@@ -8,3 +8,9 @@ For example, if the user types in 4, the program should print 100 (since 1^3 + 2
 """
 
 #write your code here
+number = int(input("Enter a positive integer: "))
+cubes = 0
+for c in range(1, number + 1, 1):
+  cubes = cubes + c**3
+
+print(cubes)
